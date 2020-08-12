fd77d488b3b3491f017278a11f2dfec8c08cad78
colrick <colrick.smith82@gmail.com>
2020-08-12 13:50:42 +0200

    4 files modified, 3 added

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Input} from '@angular/core';
 import {Output, EventEmitter} from '@angular/core';
+
+
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
